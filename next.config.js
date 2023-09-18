dbfe2fa2c6bd1f102d0469a2af1655f5fd1c34bb
aaliyahsymlar <asym14@gmail.com>
2023-09-18 16:09:51 -0500

    ignore ts errors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
     remotePatterns: [
@@ -28,9 +28,6 @@ const nextConfig = {
         hostname: "placehold.co",
       },
     ],
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
 };
 
